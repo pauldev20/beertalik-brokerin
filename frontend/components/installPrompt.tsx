9c8d4ef5f31e10df2b6347f045e7a23892456420
Paul Geeser <commit@pauldev.sh>
2024-10-20 03:39:46 -0700

    hopefully last fix for installPrompt

--- frontend/components/installPrompt.tsx
+++ frontend/components/installPrompt.tsx
@@ -8,44 +8,47 @@ export default function InstallPrompt({ children }: Readonly<{ children: React.R
     const [isStandalone, setIsStandalone] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
     const [isIOS, setIsIOS] = useState(false);
-   
+
     useEffect(() => {
         setIsIOS(
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
         )
         setIsStandalone(window.matchMedia('(display-mode: standalone)').matches)
-
+    }, []);
+    useEffect(() => {
         const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-        setIsMobile(mobileRegex.test(navigator.userAgent) || window.innerWidth < 768);
+        // setIsMobile(mobileRegex.test(navigator.userAgent) || window.innerWidth < 768);
+        setIsMobile(true);
     }, []);
 
-    return children;
     if (isStandalone) {
         return children;
     }
 
     return (
-        <div className="h-screen w-screen flex flex-col gap-4 items-center justify-center">
+        <div className="h-screen w-screen flex flex-col gap-4 items-center justify-center p-3">
             <h3 className="text-2xl font-bold">🍺 Install App 🍺</h3>
 
-            {!isMobile && (<p>
+            {!isMobile && (<p className="text-center">
                 This site is intended for mobile devices only. Please visit us on a mobile device for the best experience.
             </p>)}
 
-            {(isIOS && isMobile) && (
+            {(!isIOS && isMobile) && (
                 <Button>Add to Home Screen</Button>
             )}
 
-            {(!isIOS && isMobile) && (
+            {(isIOS && isMobile) && (
                 <p className="flex flex-col items-center justify-center text-center gap-2">
-                    <span className="flex items-center">
+                    <span>
                         To install this app on your iOS device, tap the share button
-                        <ArrowUpOnSquareIcon className="size-5 mx-1" />
-                    </span>
-                    <span className="flex items-center">
-                        {"and then \"Add to Home Screen\""}
-                        <PlusIcon className="size-5 mx-1" />
+                        <span className="inline-flex items-center align-middle">
+                            <ArrowUpOnSquareIcon className="size-5 mx-1" />
+                        </span>
+                        and then "Add to Home Screen"
+                        <span className="inline-flex items-center align-middle">
+                            <PlusIcon className="size-5 mx-1" />
+                        </span>
                     </span>
                 </p>
             )}
