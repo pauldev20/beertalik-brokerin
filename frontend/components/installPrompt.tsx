9e3f69cce261f1196f1ba50273bc41ffca3b91be
Paul Geeser <commit@pauldev.sh>
2024-10-20 02:01:27 -0700

    improved installPrompt

--- frontend/components/installPrompt.tsx
+++ frontend/components/installPrompt.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { ArrowUpOnSquareIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { Button } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
 export default function InstallPrompt({ children }: Readonly<{ children: React.ReactNode }>) {
-    const [isIOS, setIsIOS] = useState(false)
-    const [isStandalone, setIsStandalone] = useState(false)
+    const [isStandalone, setIsStandalone] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+    const [isIOS, setIsIOS] = useState(false);
    
     useEffect(() => {
         setIsIOS(
@@ -12,31 +15,35 @@ export default function InstallPrompt({ children }: Readonly<{ children: React.R
             /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
         )
         setIsStandalone(window.matchMedia('(display-mode: standalone)').matches)
-    }, [])
-   
-    return children;
+
+        const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        setIsMobile(mobileRegex.test(navigator.userAgent) || window.innerWidth < 768);
+    }, []);
+
     if (isStandalone) {
         return children;
     }
-   
+
     return (
-        <div>
-          <h3>Install App</h3>
-          <button>Add to Home Screen</button>
-          {isIOS && (
-            <p>
-              To install this app on your iOS device, tap the share button
-              <span role="img" aria-label="share icon">
-                {' '}
-                ⎋{' '}
-              </span>
-              {"and then \"Add to Home Screen\""}
-              <span role="img" aria-label="plus icon">
-                {' '}
-                ➕{' '}
-              </span>.
-            </p>
-          )}
+        <div className="h-screen w-screen flex flex-col gap-4 items-center justify-center">
+            <h3 className="text-2xl font-bold">🍺 Install App 🍺</h3>
+
+            {!isMobile && (<p>
+                This site is intended for mobile devices only. Please visit us on a mobile device for the best experience.
+            </p>)}
+
+            {(!isIOS && isMobile) && (
+                <Button>Add to Home Screen</Button>
+            )}
+
+            {(isIOS && isMobile) && (
+                <p className="flex items-center">
+                    To install this app on your iOS device, tap the share button
+                    <ArrowUpOnSquareIcon className="size-5 mx-1" />
+                    {"and then \"Add to Home Screen\""}
+                    <PlusIcon className="size-5 mx-1" />
+                </p>
+            )}
         </div>
     )
 }
