0bdf7c72486b765e9e50c24abec6d33ab312a4ee
Paul Geeser <commit@pauldev.sh>
2024-10-20 02:06:37 -0700

    animated emoji

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <main className="h-[100svh] w-full flex flex-col items-center justify-between">
             <div className="flex-1 flex flex-col items-center justify-center">
                 <div className="flex flex-col gap-3 items-center">
-                    <span style={{fontSize: "200px", lineHeight: "200px"}}>🍺</span>
+                    <span className="animate-wave origin-wave-origin" style={{fontSize: "200px", lineHeight: "200px"}}>🍺</span>
                     <h1 className="text-4xl font-bold">{siteConfig.name}</h1>
                 </div>
             </div>
